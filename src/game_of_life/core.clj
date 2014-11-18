7f6cd8c235c838ee1b507766e89ee53096f596c5
trikitrok <trikitrok@gmail.com>
2014-11-18 21:03:22 +0100

    We can know the neighbors of a cell as long as it is at [1 1]

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -2,3 +2,6 @@
 
 (defn is-alive? [cell living-cells]
   (= cell (some #{cell} living-cells)))
+
+(defn neighbors [cell]
+  #{[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]})
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -8,4 +8,7 @@
        
        (fact "we can know if a cell is not alive"
              (is-alive? [1 2] [[1 25] [4 5]]) => false)
+       
+       (fact "we can know the neighbors of a cell"
+             (neighbors [1 1]) => #{[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]})
 )
